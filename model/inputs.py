e11d2c9a7d54c68420a0c40b836e2ff264c005bd
srullsim <simchonisrael@gmail.com>
2025-07-13 10:40:33 +0300

    restart project

---
+++ model/inputs.py
@@ -0,0 +1,34 @@
+from model import Model
+from classify import ProbabilityCalculater
+
+
+
+class Inputs:
+    def __init__(self,model):
+        self.model = model
+        self.dict_values = self.model.dict_values()
+        self.calculate = ProbabilityCalculater(self.model)
+
+    def create_dict(self):
+        column_options = list(self.dict_values.values())[0]
+        input_dict = {}
+        for k, v in column_options.items():
+            options = dict(enumerate(list(v.keys())))
+            choice = input(f"{k}:\n {options}\n")
+            try:
+                if int(choice) in options.keys():
+                    input_dict[k] = options[int(choice)]
+                else:
+                    print("Not a valid option please start again")
+                    return self.create_dict()
+            except Exception as ex:
+                print("error: ", ex)
+                return self.create_dict()
+        return input_dict
+
+    def probability(self):
+        return self.calculate.probability(self.create_dict())
+
+# a = Model(r'C:\Users\User\Desktop\DATA\NaiveBayes\data_for_NB_buys_computer-Sheet1.csv','Buy_Computer','id')
+# b = Inputs(a)
+# print(b.probability())
--- model/trainer.py
+++ model/trainer.py
@@ -1,34 +1,47 @@
-
 import pandas as pd
 from pprint import  pprint
-import classify
-from model.classify import Classify
-from model.cleaner import clean_data
+from model import Model
+from classify import ProbabilityCalculater
 
 
 class Trainer:
 
-    def __init__(self, path, target):
-        # self.model = Classify()
-        # self.df = self.model.from_data_to_dict(path, target)
-        # self._dict_columns = self.set_dict_columns(self.df)
-        pass
+    def __init__(self,model):
+        self.model =model
+        self.probable = ProbabilityCalculater(self.model)
 
-    def set_dict_columns(self, df):
-        columns = {}
-        for col in df[-1].unique():
-            if col not in columns.keys():
-                columns[col] = 1
+    def sample(self,fraction):
+        return self.model.DF.sample(frac=fraction)
+
+    @staticmethod
+    def left_sample(sample,original):
+        return original[~original.index.isin(sample.index)]
+
+    @staticmethod
+    def data_frame_to_dict(dataframe):
+        return dataframe.to_dict('index')
+
+
+    def test(self):
+        right = 0
+        wrong = 0
+        test_sample = self.sample(0.7)
+        original_dataframe = self.model.DF
+        self.model.DF = test_sample
+        trial_data = self.left_sample(test_sample,original_dataframe)
+        for k,v in self.data_frame_to_dict(trial_data.drop(self.model.column_to_work_on,axis=1)).items():
+            if str(self.probable.probability(v)) == str(trial_data.loc[k,self.model.column_to_work_on]):
+                right += 1
             else:
-                columns[col] += 1
-        return columns
-
-    def check_not_zero(self, val):
-        if val != 0:
-            return True
-        return False
+                wrong += 1
+        return right / (right + wrong)
 
 
+# model = Model(r'C:\Users\User\Desktop\DATA\NaiveBayes\data_for_NB_buys_computer-Sheet1.csv','Buy_Computer','id')
+#
+# tester = Tester(model)
+#
+# print(tester.test())
 
 
 
